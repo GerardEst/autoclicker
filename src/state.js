b1be452be77f0e0d28e66a01f32e69eb7d001ba2
Gerard Esteve <gesteve.12@gmail.com>
2022-12-01 16:41:57 +0100

    Clean state

--- src/state.js
+++ src/state.js
@@ -1,38 +1,10 @@
-/** El estado del juego vive aquí
- * Siempre que algo deba afectar al estado, es tan facil como
- * importarlo y usar el metodo que se necesite.
- *
- * No se puede alterar directamente el estado por evitar algun
- * desastre, todo se hace a través de funciones que alteran el
- * estado de una forma conocida.
- *
- * Separando esto, facilitamos que mas adelante se pueda usar
- * una db real ya que solo habria que modificar estas funciones
- *
- * Tambien me servirá para guardar las cosas en localStorage
- */
-
-// Si al iniciar vemos que hay un currentUser seteado en LS, debemos cargar lo suyo
-// DUDO DE SI PONER ESTO AQUI. O DE COMO HACERLO EN GENERAL.
-// ADEMAS, STATE DEBE SER UN SINGLETON. PERO NO IMPORTA PORQUE ESTO ES UN MODULO Y SE TRATA COMO TAL
-
-// Algunas veces usas function y otras consts? De que vas?
-
 const state = {
   currentPlayer: '',
   players: {},
 };
 
-function initState() {
-  // Inicia el estado segun la info de localStorage
-
-  let storageData = JSON.parse(localStorage.getItem('autoclicker'));
-  if (storageData) {
-    state.currentPlayer = storageData.currentPlayer;
-    state.players = storageData.players;
-  }
-}
-initState();
+let localStorageData = JSON.parse(localStorage.getItem('autoclicker'));
+if (localStorageData) initStateFromLocalStorage();
 
 export const getCurrentPlayer = () => state.currentPlayer;
 export const getPlayer = name => state.players[name];
@@ -40,9 +12,6 @@ export const getAllPlayers = () => state.players;
 
 export const setCurrentPlayer = currentPlayer => {
   state.currentPlayer = currentPlayer;
-
-  // Molaria poner algo en el objeto para que simplemente se diera cuenta de que cambia y
-  // automaticamente actualizara LS
   saveChangesToLocalstorage();
 };
 
@@ -52,16 +21,12 @@ export const addNewPlayer = name => {
     upgrades: 0,
   };
   state.players[name] = newPlayerStats;
-
   saveChangesToLocalstorage();
 };
 
 export const alterPlayer = (player, prop, value) => {
   let playerToAlter = state.players[player];
   playerToAlter[prop] = value;
-
-  // Aqui no sé si esto deberia hacerlo menos a menudo, porque cambiar el LS cada pocos milisegundos
-  // sin parar yo que se
   saveChangesToLocalstorage();
 };
 
@@ -69,6 +34,11 @@ export const clearCurrentPlayer = () => {
   state.currentPlayer = '';
   saveChangesToLocalstorage();
 };
+
+function initStateFromLocalStorage() {
+  state.currentPlayer = localStorageData.currentPlayer;
+  state.players = localStorageData.players;
+}
 
 function saveChangesToLocalstorage() {
   localStorage.setItem('autoclicker', JSON.stringify(state));
