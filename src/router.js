9cf651bad54eaf26cefcce7df4148adee6fd4d21
Gerard Esteve <gesteve.12@gmail.com>
2022-12-02 13:07:11 +0100

    clean routter

--- src/router.js
+++ src/router.js
@@ -3,44 +3,19 @@ const routes = {
   '/game': 'game-page',
 };
 
-/** Todo lo que no vaya a index va aquí
- * Desde aqui tenemos que ver si es /game (o alguna otra posible)
- * Si lo es, deberiamos conseguir ir a / (index) y hacer goTo
- *    O hacer directamente goTo pero luego estamos "viviendo" en 404 y no puede ser
- *
- * Habria que redirigir siempre a la base pero recordar donde estabamos y hacer handleLocation luego...
- *
- */
-// Al inicio, comprovar ruta
+if (document.title === '404') dealWith404();
 function dealWith404() {
   let url = window.location.pathname;
-
-  // Contant que sempre va a 404,
   if (!routes[url]) {
-    // Si es 404, va a home
-    // Aixo perfecte
     window.location.replace('/');
   } else {
-    // Si existeix, li afegim un hash per indicar que vé de 404
     window.location.replace(url.replace('/', '/#'));
   }
 }
-if (document.title === '404') dealWith404();
-
-export function goTo(destiny) {
-  if (!routes[destiny]) destiny = '/';
-
-  history.pushState({page: destiny}, destiny, destiny);
-  handleLocation();
-}
 
 function handleLocation() {
-  // Si recibo una url que tiene #, significa que viene de 404 y lo que va detras es la pagina que pretendia cargar
-  // Entonces molaria quitar el # y cargar el componente, pero con cargar el componente me conformo
-
   let path = window.location.pathname;
 
-  // Si se esta mandando desde 404 tiene un hash:
   let hash = window.location.hash;
   if (hash) path = hash.replace('#', '/');
 
@@ -50,6 +25,13 @@ function handleLocation() {
 
   document.querySelector('body').innerHTML = '';
   document.querySelector('body').appendChild(newPage);
+}
+
+export function goTo(destiny) {
+  if (!routes[destiny]) destiny = '/';
+
+  history.pushState({page: destiny}, destiny, destiny);
+  handleLocation();
 }
 
 onpopstate = () => handleLocation();
